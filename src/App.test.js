b5ca172471a2193c1fa0febd51f71e946fd3db89
SukirtiSaxena <sukirtisaxena@gmail.com>
2021-12-02 21:08:38 +0000

    Error test Cases running fine

--- src/App.test.js
+++ src/App.test.js
@@ -22,15 +22,40 @@ beforeAll(() => server.listen())
 afterEach(() => server.resetHandlers())
 afterAll(() => server.close())
 
+describe('Films', () => {
 
-test("renders 'Title of the movie is' text", () => {
-  render(<App />);
-  expect(screen.getByText(/Title of the movies is/i)).toBeInTheDocument();
-});
+  test("renders 'Title of the movie is' text", () => {
+    render(<App />);
+    expect(screen.getByText(/Title of the movies is/i)).toBeInTheDocument();
+  });
 
-// Why this test case is failing????? *******************
- test('Display the title of first movie', async () => {
-  render(<App />);
+  // Why this test case is failing????? *******************
+  test('Display the title of first movie', async () => {
+    render(<App />);
     await waitFor(() => screen.getByText(/Castle in the Sky/i));
     expect(screen.getByText(/Castle in the Sky/i)).toBeInTheDocument();
-  }); 
+  });
+
+  test('If there is error response with status code 500, display error message', async () => {
+    server.use(
+      rest.get("https://ghibliapi.herokuapp.com/films", (req, res, ctx) => {
+        return res(ctx.status(500));
+      })
+    );
+    render(<App />);
+    await waitFor(() => screen.getByText(/Oops… something went wrong, try again/i));
+    expect(screen.getByText(/Oops… something went wrong, try again/i)).toBeInTheDocument();
+  });
+
+  test('If there is error response with status code 418, display error message', async () => {
+    server.use(
+      rest.get("https://ghibliapi.herokuapp.com/films", (req, res, ctx) => {
+        return res(ctx.status(418));
+      })
+    );
+    render(<App />);
+    await waitFor(() => screen.getByText(/418 I'm a tea pot/i));
+    expect(screen.getByText(/418 I'm a tea pot/i)).toBeInTheDocument();
+  });
+
+});
