5881f7a89b04c13014c7d783293ac5cf1138748d
SukirtiSaxena <sukirtisaxena@gmail.com>
2021-12-02 20:54:41 +0000

    one failing test for title of first movie

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,36 @@
-import { render,  screen } from '@testing-library/react';
+import { render, waitFor, screen } from '@testing-library/react';
 import App from './App';
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 
-test('renders learn react link', () => {
+const server = setupServer(
+  rest.get('https://ghibliapi.herokuapp.com/films', (req, res, ctx) => {
+    return res(ctx.json({
+      data: [{
+        id: 1,
+        title: "Castle in the Sky"
+      },
+      {
+        id: 2,
+        title: "Frozen"
+      }]
+    }))
+  }),
+);
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
+
+
+test("renders 'Title of the movie is' text", () => {
   render(<App />);
-  const titleText = screen.getByText(/Title of the movies is/i);
-  expect(titleText).toBeInTheDocument();
+  expect(screen.getByText(/Title of the movies is/i)).toBeInTheDocument();
 });
+
+// Why this test case is failing????? *******************
+ test('Display the title of first movie', async () => {
+  render(<App />);
+    await waitFor(() => screen.getByText(/Castle in the Sky/i));
+    expect(screen.getByText(/Castle in the Sky/i)).toBeInTheDocument();
+  }); 
