d4a326811f98e52ab22587905e0eafe20a7f8c91
SukirtiSaxena <sukirtisaxena@gmail.com>
2021-12-02 12:27:42 +0000

    test if component is rendered on screen

---
+++ src/Components/Title.test.js
@@ -0,0 +1,12 @@
+import { render,screen } from "@testing-library/react"
+import Title from "../Components/Title.js"
+
+describe("Title of the Film",()=>{
+    test('renders title of the first film',() =>{
+        const propTitle={
+            title : "Castle in the Sky"
+        };
+        render(<Title {...propTitle}/>);
+        expect(screen.getByText(/Castle in the Sky/i)).toBeInTheDocument();
+    });
+});
