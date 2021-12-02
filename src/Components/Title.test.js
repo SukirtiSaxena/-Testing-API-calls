01cbed18aad589b0f645f479586186082c2c455b
SukirtiSaxena <sukirtisaxena@gmail.com>
2021-12-02 14:56:03 +0000

    fixed index.html

--- src/Components/Title.test.js
+++ src/Components/Title.test.js
@@ -1,12 +1,34 @@
-import { render,screen } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import Title from "../Components/Title.js"
 
-describe("Title of the Film",()=>{
-    test('renders title of the first film',() =>{
-        const propTitle={
-            title : "Castle in the Sky"
+describe("Title of the Film", () => {
+    test('renders title of the first film', () => {
+        const propTitle = {
+            title: "Castle in the Sky"
         };
-        render(<Title {...propTitle}/>);
+        render(<Title {...propTitle} />);
         expect(screen.getByText(/Castle in the Sky/i)).toBeInTheDocument();
     });
+
+    test('Returns the title of the first film', async () => {
+        const getFilms = require('../App');
+        const axios = require('axios');
+        jest.mock('axios');
+        axios.get.mockResolvedValue({
+            data: [{
+                id: 1,
+                title: "Castle in the Sky"
+            },
+            {
+                id: 2,
+                title: "Frozen"
+            }]
+        });
+
+        const title = await getFilms();
+        expect(title).toEqual('Castle in the Sky');
+        jest.resetAllMocks();
+    });
+
 });
+
